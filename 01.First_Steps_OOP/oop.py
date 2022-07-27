4a47b271d84923cacc6f146ba4347fe82099ab94
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-07-27 21:05:14 +0300

    Add files via upload

---
+++ 01.First_Steps_OOP/oop.py
@@ -0,0 +1,20 @@
+"""
+The most popular programming paradigm
+Concept of classes and object
+Clear program structure
+Reusable code
+Easy maintenance and modification
+Reduce complexity
+
+Everything in python is an object
+Object - data abstraction that captures a value and an interface ( functionality )
+Object - state + behavior
+
+Creating our custom types -> Classes
+"""
+
+
+class Person:
+    def __init__(self):  # Setting the state
+        self.name = "Kristian"  # Attributes
+        self.age = 28
