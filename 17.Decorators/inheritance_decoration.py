9656ed4ba0807715f04c8250c783dd8e9d8b48b9
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-10 20:06:20 +0300

    Add files via upload

---
+++ 17.Decorators/inheritance_decoration.py
@@ -0,0 +1,25 @@
+class Person:
+    pass
+
+
+class Car:
+    pass
+
+
+class Dog:
+    pass
+
+
+class CarPerson(Person, Car):
+    pass
+
+
+class PersonDog(Person, Dog):
+    pass
+
+
+class CarPersonDog(Car, Person, Dog):
+    pass
+
+
+"""Inheritance explosion - Avoided by decorators"""
