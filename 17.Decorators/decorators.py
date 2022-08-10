aaab8b36d0ad5e570aec1d28b235879cf8479838
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-10 20:05:21 +0300

    Add files via upload

---
+++ 17.Decorators/decorators.py
@@ -0,0 +1,8 @@
+"""
+Decorators - Adding functionality
+- They allow modification of behavior of a function or a class
+- Decorators extend the behavior of wrapped function
+- A function getting as parameter another function
+- They take exactly one parameter and have a nested wrapper function
+
+"""
