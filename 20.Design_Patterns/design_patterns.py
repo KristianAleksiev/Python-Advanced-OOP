383de36d7f5f581e5a1e6d4866cda5464e36a911
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-14 18:33:33 +0300

    Add files via upload

---
+++ 20.Design_Patterns/design_patterns.py
@@ -0,0 +1,29 @@
+"""
+1. Definition of Design patterns
+- Type of problem, isolated and managed
+- Solutions to common problems in software design
+- A pattern for solving given problems
+- Add additional layers of abstraction on order to reach flexibility
+
+- Problem - Solution guidelines, abstract code
+
+2. Benefits and Drawbacks
+
+Benefits:
+- large-scale reuse
+- speed-up the development
+- better communication
+
+Drawbacks:
+- They solve a problem in a particular way, may not be the best way
+- Do not lead to a direct code reuse
+- Design pattern overload
+- Should be used only if understood well
+
+
+3. Types of design patterns
+- Creational
+- Structural
+- Behavioral
+
+"""
