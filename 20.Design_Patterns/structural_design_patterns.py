e01078fe2bfed7590f769f4d28d13f1fcef5283b
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-14 18:34:36 +0300

    Add files via upload

---
+++ 20.Design_Patterns/structural_design_patterns.py
@@ -0,0 +1,13 @@
+"""
+Describe ways to assemble objects to implement new functionality, composition of classes and objects
+Ease the design by indetifying a simple way to realize relationship between entities
+
+List of Structural Patterns:
+- Facade - Hidden complex logic behind a method (built-ins)
+- Composite - 
+- Flyweight
+- Proxy
+- Decorator
+- Adapter - Adapt something we take from third party to our logic
+- Bridge
+"""
