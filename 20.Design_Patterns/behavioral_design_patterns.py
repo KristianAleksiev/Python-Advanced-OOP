fcf0e07270edf4f995ca43a8efb28acf5e83e6fb
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-08-16 17:58:05 +0300

    Add files via upload

---
+++ 20.Design_Patterns/behavioral_design_patterns.py
@@ -0,0 +1,15 @@
+"""
+Deal with dynamic interactions among the different components
+- Assignment of responsibilities between objects
+- Encapsulating behavior in an object
+- Distribute responsibility
+- Flexibility in cross-classes communication
+
+List of Behavioral design patterns:
+- Chain of Responsibility
+- Iterator
+- Command - Wraps a function in an object, so we can use it
+- Template Method
+- Strategy
+- Observer
+"""
